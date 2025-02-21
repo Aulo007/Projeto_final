90ee259b046754bc3c7f6feec3a41c167e78d81b
Aulo Cezar <Aulofferreira@gmail.com>
2025-02-21 20:50:48 -0300

    Implementação de interrupções, configuração de joystick e melhorias no
    - Criação das interrupções necessárias.
    - Configuração do joystick para simulação de sensores.
    - Ajustes na lógica do loop principal.
    - Configuração de mensagens necessárias para o funcionamento.
    - Implementação da funcionalidade de armazenamento de imagens em uma biblioteca para uso no código principal.

--- BIBLIOTECAS/bitmaps.h
+++ BIBLIOTECAS/bitmaps.h
@@ -1,6 +1,9 @@
-#ifndef DESENHO_H
-#define DESENHO_H
+#ifndef BITMAPS_H
+#define BITMAPS_H
 
-extern int caixa_de_desenhos[10][5][5][3];
+#include "pico/stdlib.h"
 
-#endif // DESENHO_H
+
+extern const uint8_t mensagem_sem_calibrar[]; // Mensagem a ser exibida quando o usuário não calibrou os sensores
+
+#endif // BITMAPS_H
